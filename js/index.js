4d3905dbea1ed57a56a649ac0863bfe7de3e4b51
TimKabue <39269490+TimKabue@users.noreply.github.com>
2022-10-24 20:51:43 +0300

    Initial commit.
    Initial files for the project were uploaded.

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//Attach event listener to all buttons on the document.
+for(element of document.querySelectorAll(".navbar-items")){
+  element.addEventListener("click",function(){
+    alert("This is a demonstration website. This function is yet to be implemented.");
+  });
+}
+
+for(element of document.querySelectorAll(".btn")){
+  element.addEventListener("click",function(){
+    alert("This is a demonstration website. This function is yet to be implemented.");
+  });
+}
